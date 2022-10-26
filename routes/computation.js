1f50695b299574d486a5e93677c77fe3819b6f73
Divya Sree Vadlakonda <divyasreev155@gmail.com>
2022-10-26 17:21:15 -0500

    adding the updated bonus file to git

---
+++ routes/computation.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    var variable1 = Math.random();
+    var variable2 = Math.fround(variable1);
+    var variable3 = Math.random(variable1);
+    var variable4 = Math.round(variable1);
+  res.render('computation', { title: 'Math.fround applied to ' + variable1 + 'is' + variable2},
+  { title: 'Math.random applied to ' + variable1 + 'is' + variable3},
+  { title: 'Math.round applied to ' + variable1 + 'is' + variable4})
+});
+
+module.exports = router;
