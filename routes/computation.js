a8e50064710bdf3b2408ede57a2032179b0a3d53
Divya Sree Vadlakonda <divyasreev155@gmail.com>
2022-10-26 17:36:53 -0500

    updated bonus

--- routes/computation.js
+++ routes/computation.js
@@ -3,13 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var variable1 = Math.random();
+    var variable1 = Math.floor(Math.random()*30);
     var variable2 = Math.fround(variable1);
     var variable3 = Math.random(variable1);
     var variable4 = Math.round(variable1);
-  res.render('computation', { title: 'Math.fround applied to ' + variable1 + 'is' + variable2},
-  { title: 'Math.random applied to ' + variable1 + 'is' + variable3},
-  { title: 'Math.round applied to ' + variable1 + 'is' + variable4})
+  res.render('computation', { title: 'Math.fround() applied to ' + variable1 + 'is' + variable2 },
+  { title1: 'Math.random() applied to ' + variable1 + 'is' + variable3 },
+  { title2: 'Math.round() applied to ' + variable1 + 'is' + variable4 })
 });
 
 module.exports = router;
