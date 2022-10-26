acf4ba7c308f238de8cbe614bf6b14f2c831537e
Divya Sree Vadlakonda <divyasreev155@gmail.com>
2022-10-26 18:05:37 -0500

    updated the bonus to computation

--- routes/computation.js
+++ routes/computation.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     var variable1 = Math.floor(Math.random()*30);
     var variable2 = Math.fround(variable1);
-    var variable3 = Math.random();
+    var variable3 = Math.floor(Math.random() * variable1);
     var variable4 = Math.round(variable1);
   res.render('computation', {title: 'Divya Sree Vadlakonda bonus page', title1: 'Math.fround() applied to ' + variable1 + ' is ' +  variable2,
    title2: 'Math.random() applied to ' + variable1 + ' is ' +  variable3,  
